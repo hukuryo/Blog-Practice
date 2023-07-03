094abec6365d6ec4c34b2bff1624ae09cfea4269
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-03 21:36:50 +0900

    サイドバーを作成

--- next-blog/app/page.tsx
+++ next-blog/app/page.tsx
@@ -2,13 +2,17 @@
 
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { SideBar } from "./components/SideBar";
 
 export default function Home() {
   return (
     <>
-      <div className="relative">
-        <Header/>
+      <Header/>
+      <div className="relative flex">
         <h1>ホームページです</h1>
+      </div>
+      <div>
+        <SideBar/>
       </div>
       <Footer/>
     </>
