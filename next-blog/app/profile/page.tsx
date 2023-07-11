b23737c7fc1119ede86325b9631eb059a888c23e
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-11 21:06:13 +0900

    ヘッダーを改良

--- next-blog/app/profile/page.tsx
+++ next-blog/app/profile/page.tsx
@@ -7,18 +7,19 @@ import { SideBar } from "../components/SideBar";
 export default function profile() {
     return (
         <>
-        <Header/>
-        <main className="min-h-screen bg-gray-100">
-            <SideBar/>
-            <div className="max-w-md mx-auto bg-white rounded-lg shadow-md overflow-hidden">
-                    <div className="flex">
-                        <div className="relative flex">
-                            <h1>プロフィールページです</h1>
+            <Header/>
+            <main className="container mx-auto">
+                <div className="flex">
+                    <SideBar/>
+                    <div className="ml-64 mt-32">
+                        <div className="flex">
+                            <div className="relative flex">
+                                <h1>プロフィールページです</h1>
+                            </div>
                         </div>
                     </div>
-            </div>
-        </main>
-        <Footer/>
+                </div>
+            </main>
         </>
     )
 }
