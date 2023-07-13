81de2e83518bb8ae0dee8822171335ef6013d7d9
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-13 22:05:13 +0900

    プロフィールページを編集

--- next-blog/app/components/SideBar.tsx
+++ next-blog/app/components/SideBar.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 export function SideBar() {
     return (
         <>
-            <aside id="cta-button-sidebar" className="h-full w-2/5 h-screen transition-transform pl-5" aria-label="Sidebar" aria-hidden="true">
-                <div className="shadow-lg h-full px-3 py-4 bg-indigo-100 dark:bg-gray-800">
-                    <div id="dropdown-cta" className="p-4 mt-5 mb-5 rounded-lg bg-blue-50 dark:bg-blue-900" role="alert">
+            <aside id="cta-button-sidebar" className="h-full w-2/5 transition-transform pl-5" aria-label="Sidebar" aria-hidden="true">
+                <div className="shadow-lg h-full px-3 py-4 bg-indigo-100 dark:bg-gray-700">
+                    <div id="dropdown-cta" className="p-4 mt-5 mb-5 rounded-lg bg-blue-50 dark:bg-blue-500" role="alert">
                         <div className="flex items-center justify-center mb-3">
                         </div>
                         <h3 className="text-center mt-7 mb-7">fukuryo</h3>
--- next-blog/app/profile/page.tsx
+++ next-blog/app/profile/page.tsx
@@ -11,14 +11,17 @@ export default function profile() {
             <main className="md:container md:mx-auto">
                 <div className="flex flex-row-reverse">
                     <SideBar/>
-                    <div className="mt-16 ml-16 w-full">
+                    <div className="mt-16 ml-16 w-full rounded-md">
                         <h1 className="font-bold text-2xl">プロフィール紹介</h1>
                         <p className="mt-16">Ryohei-no-Blogへようこそ！</p>
                         <p className="mt-8">このブログでは技術についての記事などを中心に記事を書いています。</p>
                         <p className="mt-8">このページでは自分の経歴を紹介していきます。</p>
                         <div className="mt-32">
-                            <div className="border border-slate-300 h-80 w-5/6 px-8 pt-8">
-                                <p>2021/</p>
+                            <div className="border border-slate-300 h-80 w-5/6 px-8 pt-8 mb-10">
+                                <h5 className="text-center mb-7 font-bold">経歴紹介</h5>
+                                <p className="mb-5 border-b-8">2021/4~鉄道会社に就職</p>
+                                <p className="mb-5 border-b-8">2022/11~エンジニアに転職</p>
+                                <p className="border-b-8">官公庁向けのシステム開発に携わる。</p>
                             </div>
                         </div>
                     </div>
