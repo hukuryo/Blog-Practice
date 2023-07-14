258ecf0630f112eaac39a8f31ce653c1a224761e
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-14 21:04:54 +0900

    お問合せフォームを作成

--- next-blog/app/profile/page.tsx
+++ next-blog/app/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
+import { Footer } from "../components/FooterForm";
 import { SideBar } from "../components/SideBar";
 
 export default function profile() {
@@ -19,11 +19,16 @@ export default function profile() {
                         <div className="mt-32">
                             <div className="border border-slate-300 h-80 w-5/6 px-8 pt-8 mb-10">
                                 <h5 className="text-center mb-7 font-bold">経歴紹介</h5>
-                                <p className="mb-5 border-b-8">2021/4~鉄道会社に就職</p>
-                                <p className="mb-5 border-b-8">2022/11~エンジニアに転職</p>
+                                <p className="mb-7 border-b-8">2021年3月 大学卒業</p>
+                                <p className="mb-7 border-b-8">2021年4月 鉄道会社に就職</p>
+                                <p className="mb-7 border-b-8">2022年11月 エンジニアに転職</p>
                                 <p className="border-b-8">官公庁向けのシステム開発に携わる。</p>
                             </div>
+                            <p className="mb-10">千葉県民です。今も千葉県在住です。</p>
+                            <p className="mb-10">大学まで野球をやっていました。</p>
+                            <p>趣味はスポーツ観戦、映画鑑賞、漫画を読むこと</p>
                         </div>
+                    <Footer />
                     </div>
                 </div>
             </main>
