1a4c6e05a32bc72c5c4a6f4f1368c17287a8b0d8
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-24 22:00:34 +0900

    ヘッダーのデザインを修正

--- next-blog/app/components/Header.tsx
+++ next-blog/app/components/Header.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 export function Header() {
     return (
         <>
-            <div className="w-full shadow-lg  p-5 pl-5 border border-gray-200 bg-sky-200 rounded-t-xl dark:border-gray-600 dark:bg-gray-700">
+            <div className="w-full shadow-lg  p-4 pl-4 border border-gray-200 bg-sky-200 rounded-t-xl dark:border-gray-600 dark:bg-gray-700">
                 <div className="grid grid-cols-3">
                     <div className="sm:col-span-1">
                         <h1 className="font-bold text-xl">Hukuryo-no-Blog</h1>
                     </div>
-                    <div className="items-center justify-center hidden col-span-1 space-x-3 sm:flex">
-                        <Link className="text-xs hover:border-black" href="/">トップページ</Link>
-                        <Link className="text-xs hover:border-black" href="/articleList">記事一覧</Link>
-                        <Link className="text-xs hover:border-black" href="/profile">プロフィール</Link>
-                        <Link className="text-xs hover:border-black" href="/bookIntroduction">IT書籍紹介</Link>
+                    <div className="items-center justify-center hidden col-span-1 space-x-3 sm:flex w-96">
+                        <Link className="text-xs hover:border-black font-bold hover:border-b-2" href="/">トップページ</Link>
+                        <Link className="text-xs hover:border-black font-bold hover:border-b-2" href="/articleList">記事一覧</Link>
+                        <Link className="text-xs hover:border-black font-bold hover:border-b-2" href="/profile">プロフィール</Link>
+                        <Link className="text-xs hover:border-black font-bold hover:border-b-2" href="/bookIntroduction">IT書籍紹介</Link>
                     </div>
                     <div className="flex justify-end col-span-2">
                         <button data-drawer-target="cta-button-sidebar" data-drawer-toggle="cta-button-sidebar" aria-controls="cta-button-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-700 rounded-lg sm:hidden bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600">
