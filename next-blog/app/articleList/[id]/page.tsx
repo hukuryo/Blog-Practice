05ab84bc01d9bc63df78092ea616aa7f55a2293b
hukuryo <ryohei.1998.05.09@gmail.com>
2023-08-04 22:07:07 +0900

    [id].tsxファイルを作成

---
+++ next-blog/app/articleList/[id]/page.tsx
@@ -0,0 +1,13 @@
+export async function generateStaticParams() {
+    return [{ id: "1" }, { id: "2" }]
+}
+
+
+export default async function Post() {
+    
+    return(
+        <>
+            <p>ArticleListDetail</p>
+        </>
+    )
+}
